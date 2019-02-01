239ad669e596760b8922ae546e02845770b73ec6
jpurata-azteca <47125395+jpurata-azteca@users.noreply.github.com>
2019-02-01 11:51:20 -0600

    Add files via upload
    first commit initial map

---
+++ js/loadcountries.js
@@ -0,0 +1,20 @@
+function loadCountries() {
+    var countries = [{
+        id: 1,
+        name: 'Tienda'
+    } , {
+        id: 2,
+        name: 'Banco Azteca / Elektra / Presta Prend'
+    },{
+        id: 3,
+        name: 'Salinas y Rocha'
+    }, {
+        id: 4,
+        name: 'Bodega de remates'
+    }, {
+        id: 5,
+        name: 'Multiva'
+    }];
+
+    return countries;
+}
